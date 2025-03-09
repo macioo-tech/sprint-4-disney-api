5d1d439866bac4670b5a6232cf2155510b86e89e
mac <orno.morsoft@gmail.com>
2025-03-09 15:37:46 +0100

    styling List Element

--- src/components/Characters/ListElement.jsx
+++ src/components/Characters/ListElement.jsx
@@ -1,3 +1,34 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: grid;
+    grid-template-columns: 200px 200px 200px;
+    border-radius: 5px;
+    padding: 5px;
+    background-color: #fff;
+    border: 2px solid #ed03ed;
+    align-items: center;
+
+`
+
+const Name = styled.h2`
+ font-size: 16px;
+ font-weight: bold;
+ color: #000;
+    
+`
+
+const ImageContainer = styled.div`
+  max-width: 400px;
+  overflow: auto;
+  resize: horizontal;
+`;
+
+const Image = styled.img`
+  max-width: 100%;
+  display: block;
+`;
+
 const ListElement = ({ item }) => {
   const { name, imageUrl } = item || {};
 
@@ -5,11 +36,13 @@ const ListElement = ({ item }) => {
   console.log('imageUrl', imageUrl)
 
   return (
-    <div>
-      <p>{name}</p>
-      <p>{imageUrl}</p>
+    <Container>
+      <Name>{name}</Name>
+      <ImageContainer>
+        <Image src={imageUrl} />
+      </ImageContainer>
       <button>Learn</button>
-    </div>
+    </Container>
   );
 };
 
